3d75e3f6f779926d04eb35a2833307856cced5fe
TinaTop <48864549+TinaTop@users.noreply.github.com>
2019-04-04 23:40:17 +0200

    Add files via upload

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window);
+	sf::CircleShape circle(20.f);
+	circle.setFillColor(sf::Color(250, 50, 250));
+
+	circle.setOutlineThickness(50.f);
+	circle.setOutlineColor(sf::Color(150, 50, 250));
+	circle.setPosition(200, 100);
+
+	sf::RectangleShape line(sf::Vector2f(150.f, 5.f));
+	line.rotate(90.f);
+	line.setPosition(225, 190);
+	line.setFillColor(sf::Color(100, 250, 50));
+
+	sf::CircleShape circle2(20.f);
+	circle2.setFillColor(sf::Color(700, 450, 400));
+	circle2.setPosition(75, 50);
+
+	
 
 	while (window.isOpen())
 	{
@@ -15,10 +33,19 @@ int main()
 				window.close();
 		}
 
+		
+		circle2.rotate(2);
+
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw();
+		window.draw(circle);
+		window.draw(line);
+		window.draw(circle2);
 		window.display();
 	}
+	
+
+	
 
 	return 0;
 }
