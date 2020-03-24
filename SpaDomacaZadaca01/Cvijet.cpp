364d757c7f382a6a032f1301af00749197a58222
Luka Čabraja <lcabraja@racunarstvo.hr>
2020-03-24 17:42:02 +0100

    Domaća Zadaća Animirao sam cvijet i boju pozadine

---
+++ SpaDomacaZadaca01/Cvijet.cpp
@@ -0,0 +1,57 @@
+#include "Cvijet.h"
+
+Cvijet::Cvijet(sf::RenderWindow *window)
+{
+	this->window = window;
+	background.setFillColor(sf::Color(100, 150, 100));
+	background.setPosition(0.f, 0.f);
+	rect.setFillColor(green);
+	rect.setPosition(70.0, 135.0);
+
+	circle.setFillColor(sf::Color(250, 250, 150));
+	circle.setOutlineThickness(25.f);
+	circle.setOutlineColor(sf::Color(250, 50, 50));
+	circle.setOrigin(50.f, 50.f);
+	circle.setPosition(75.f, 75.f);
+
+	convex.setPointCount(4);
+	convex.setFillColor(green);
+	convex.setPoint(0, sf::Vector2f(0.f, 0.f));
+	convex.setPoint(1, sf::Vector2f(250.f, 10.f));
+	convex.setPoint(2, sf::Vector2f(120.f, 45.f));
+	convex.setPoint(3, sf::Vector2f(30.f, 50.f));
+	convex.setPosition(40.f, 550);
+	convex.rotate(-20.f);
+	convex.scale(1.5f, 1.5f);
+
+	convex2.setPointCount(4);
+	convex2.setFillColor(green);
+	convex2.setPoint(0, sf::Vector2f(0.f, 0.f));
+	convex2.setPoint(1, sf::Vector2f(150.f, 10.f));
+	convex2.setPoint(2, sf::Vector2f(120.f, 90.f));
+	convex2.setPoint(3, sf::Vector2f(30.f, 100.f));
+	convex2.setPosition(-50.f, 500.f);
+	convex2.rotate(20.f);
+}
+
+void Cvijet::draw()
+{
+	window->draw(background);
+	auto color = background.getFillColor();
+	if (up) {
+		color.r++;
+		if (color.r == 255)
+			up = !up;
+	}
+	else {
+		color.r--;
+		if (color.r == 50)
+			up = !up;
+	}
+	background.setFillColor(color);
+	circle.rotate(1.f);
+	window->draw(rect);
+	window->draw(circle);
+	window->draw(convex);
+	window->draw(convex2);
+}
---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Cvijet
+{
+	bool up = true;
+	sf::RenderWindow* window;
+	sf::Color green = sf::Color(70, 250, 70);
+	sf::RectangleShape background = sf::RectangleShape(sf::Vector2f(800.f, 600.f));
+	sf::RectangleShape rect = sf::RectangleShape(sf::Vector2f(12.f, 500.f));
+	sf::CircleShape circle = sf::CircleShape(50.f, 12.f);
+	sf::ConvexShape convex;
+	sf::ConvexShape convex2;
+public:
+	Cvijet(sf::RenderWindow *window);
+	void draw();
+};
+
