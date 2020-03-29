4b77f6293edbb377bbd293dc08746b0218530f7d
APaleka <62799750+APaleka@users.noreply.github.com>
2020-03-29 20:32:51 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		cvijet.draw();
+		window.display();
+	}
+
+	return 0;
+}
