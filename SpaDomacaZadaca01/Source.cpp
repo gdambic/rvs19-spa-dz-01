989bbabac08ff78c9863970306e52d2ecf62fcd2
Antonio Bajo <61512431+Antonio-Bajo@users.noreply.github.com>
2020-04-05 19:16:13 +0200

    Update Source.cpp

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+
 
 int main()
 {
+	sf::ContextSettings aa;
+	aa.antialiasingLevel = 8;
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window);
 
 	while (window.isOpen())
 	{
@@ -16,9 +20,11 @@ int main()
 		}
 
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw();
 		window.display();
 	}
+
+
 
 	return 0;
 }
