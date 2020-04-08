04e3878057b4734c690ca2bb70901ad726283fd4
Jure Kovačević <jkovac2@racunarstvo.hr>
2020-04-08 21:56:48 +0200

    moj prvi commit

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,11 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Cvijet");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
-
+	Cvijet cvijet(&window);
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -16,9 +18,13 @@ int main()
 		}
 
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw();
 		window.display();
+
 	}
 
 	return 0;
 }
+
+
+
