862900f0104d7c9c8aeb717b13d8f89855b582f5
Your Name <you@example.com>
2020-04-09 08:27:32 +0200

    moj prvi commit

---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+class Cvijet
+{private:
+	sf::RenderWindow* window;
+	sf::Clock clock;
+
+
+
+
+public:Cvijet(sf::RenderWindow* window);
+	  void draw() const;
+};
+
