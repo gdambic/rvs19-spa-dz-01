d41a602fa99d9b1158dd3a1ab3e9b8cf9c94d851
dsteinh <78277473+dsteinh@users.noreply.github.com>
2021-03-29 21:28:41 +0200

    Update Cvijet.cpp

--- SpaDomacaZadaca01/Cvijet.cpp
+++ SpaDomacaZadaca01/Cvijet.cpp
@@ -4,8 +4,9 @@ Cvijet::Cvijet(sf::RenderWindow& window): window(window){}
 
 void Cvijet::draw()
 {
-	sf::CircleShape shape(50.f);
-	shape.setFillColor(sf::Color(100, 250, 50));
-	window.draw(shape);
+	sf::CircleShape oblik(50.f);
+	oblik.setFillColor(sf::Color(100, 250, 50));
+	window.draw(oblik);
 
 }
+//Spriteovi -> https://www.youtube.com/watch?v=1ND_VRJChO4&ab_channel=SonarSystems
