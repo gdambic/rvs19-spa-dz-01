373aec370202a11cc4cf55f2709073c6d720d01e
Patrik Stepanic <pstepan@racunarstvo.hrgit config --global user.email pstepan@racunarstvo.hr>
2021-03-29 23:51:23 +0200

    Gotov DZ01

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,27 @@
-#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Cvijetic");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(1200.f, 1000.f));
+	window.setView(view);
+	
+	sf::Text text;
+	sf::Font font;
+	if (!font.loadFromFile("font.ttf"))
+	{
+		std::cout << "Error loading fonts." << std::endl;
+		return 1;
+	}
+	text.setFont(font);
+	text.setString("Click the left mouse button to go faster \nClick the right mouse button to go slower \n(Dont go too fast) :)");
+	text.setFillColor(sf::Color::White);
+	text.setPosition(-600.f, -500.f);
+	text.setCharacterSize(40);
+
+	Cvijet cvijet(&window);
 
 	while (window.isOpen())
 	{
@@ -15,10 +32,14 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		//cvijet.draw();
+		window.clear(sf::Color::Black);
+
+		window.draw(text);
+		cvijet.draw();
+		
 		window.display();
 	}
+	
 
 	return 0;
 }
