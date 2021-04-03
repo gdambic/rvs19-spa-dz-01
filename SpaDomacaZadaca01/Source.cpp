83a1eda9420ff9cb8d637ccfb347c02477567d52
CroTard <majeric.martin@gmail.com>
2021-04-03 18:23:49 +0200

    Gotovo sa mjuzom

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -2,10 +2,22 @@
 #include"Cvijet.h"
 #include<iostream>
 #include<SFML/System/clock.hpp>
+#include<SFML/Audio.hpp>
 
 int main()
 {
 	std::cout << std::endl << "Animacija by Martin Majeric" << std::endl;
+
+	sf::Music mjuza;
+	mjuza.openFromFile("Music Sounds Better With You_01.wav");
+
+	if (!mjuza.openFromFile("Music Sounds Better With You_01.wav"))
+	{
+		std::cout << "Error - fali mjuza" << std::endl;
+		return -1;
+	}
+
+	mjuza.play();
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Cvijet");
 	window.setFramerateLimit(60);
