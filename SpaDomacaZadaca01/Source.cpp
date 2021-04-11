dc10efe54c34addf4d354a26fed2831efa1e7c9c
Jelena Ndou <jelena.ndou@gmail.com>
2021-04-11 13:57:46 +0200

    pp

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,11 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include"Cvijet.h";
+using namespace std;
+using namespace sf;
+
+
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window);
+	sf::Clock clock1;
 
+	float brzina = 100.f;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -13,10 +22,12 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			
+
 		}
 
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw();
 		window.display();
 	}
 
