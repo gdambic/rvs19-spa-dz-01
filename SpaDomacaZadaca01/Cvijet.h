c52b111ece956584d97e6d04dfec69e715b110e3
Daniel Bele <daniel.bele@algebra.hr>
2022-03-24 06:50:36 +0100

    updates

---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+class Cvijet
+{
+private:
+	RenderWindow* window;
+	CircleShape sunce;
+	CircleShape head;
+	CircleShape eye1;
+	CircleShape eye2;
+	RectangleShape line;
+	RectangleShape weed;
+	ConvexShape leaf;
+	ConvexShape mouth;
+	Clock clock;
+
+public:
+	Cvijet(RenderWindow* window);
+	void draw();
+};
