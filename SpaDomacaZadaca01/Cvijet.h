f8d7c4f0fabbc7b860406873a68c0ee5fd8d0e02
PEROPC\Pero <petar.markota@gmail.com>
2022-04-13 21:12:23 +0200

    $git push origin master

---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class Cvijet
+{
+public:
+	Cvijet(sf::RenderWindow *window);
+	void draw();
+	
+private:
+	void draw_circle();
+	void draw_line();
+	void draw_convex();
+	sf::RenderWindow *window;
+	float elapsedTimef = float();
+};
+
