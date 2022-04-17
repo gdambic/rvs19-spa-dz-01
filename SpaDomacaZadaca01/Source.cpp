605998212f98e5806722504b35744a6b299f1386
IvanCiri <103067371+IvanCiri@users.noreply.github.com>
2022-04-17 16:24:36 +0200

    Add files via upload

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,22 +1,26 @@
 #include <SFML/Graphics.hpp>
-
+#include "Cvijet.h"
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
-
+	Cvijet cvijet(&window);
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+			while (window.pollEvent(event))
+			{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
+				
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
+			
+			}
+			
 
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw();
 		window.display();
 	}
 
