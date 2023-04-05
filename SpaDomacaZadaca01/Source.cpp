605c7b8ca3b7573b4e13c037b1b8e69534cea268
matkopl <matko.plecas@racunarstvo.hr>
2023-04-05 19:32:43 +0200

    domaci rad

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,11 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window); //stvara objekt klase cvijet koji prima window kao argument
 
 	while (window.isOpen())
 	{
@@ -14,9 +15,8 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
 		window.clear();
-		//cvijet.draw();
+		cvijet.draw(); //poziv funkcije ce nacrtati cvijet na window koristeci objekt klase cvijet
 		window.display();
 	}
 
