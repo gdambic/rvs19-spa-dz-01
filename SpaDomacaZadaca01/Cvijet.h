a9640d615b7896a075ab2f636cdd16224969e900
Milica Đukić <mil1ca@outlook.com>
2023-04-09 15:11:58 +0200

    second commit

---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Cvijet
+{
+
+private:
+	RenderWindow* window;
+
+public:
+	Cvijet(RenderWindow* window);
+	void draw();
+
+};
+
