4622a65aec58d086de201ac71b14b3cc45817017
unknown <reneovac@gmail.com>
2023-04-09 13:39:43 +0200

    first commit

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,23 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+#include <iostream>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window);
+
+	window.clear(sf::Color(173, 216, 230));
+
+	float sizedif = 0.3;
+
+	float radius = 40;
+
+	Clock clock;
+	Time t1 = microseconds(1000000);
+
+	Time elapsed1;
 
 	while (window.isOpen())
 	{
@@ -15,8 +28,19 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		//cvijet.draw();
+		elapsed1 = clock.getElapsedTime();
+
+		if (elapsed1 < t1) {
+			radius += sizedif;
+			std::cout << elapsed1.asSeconds() << std::endl;
+		}
+		else {
+			clock.restart();
+			radius = 40;
+		}
+
+		window.clear(sf::Color(173, 216, 230));
+		cvijet.draw(radius);
 		window.display();
 	}
 
