0f071e7c6b53d2256d678eb3043d927a962a6c64
ZUGA <jzugic@algebra.hr>
2024-03-10 03:17:17 +0100

    Added variables, comments

--- SpaDomacaZadaca01/Flower.h
+++ SpaDomacaZadaca01/Flower.h
@@ -1,5 +1,26 @@
 #pragma once
 class Flower
 {
+private:
+	int petal_num;			//num of petals
+	float petal_size;		//size of petals
+	float stem_size;		//size of stem
+	float center_size;		//size of center
+	int leaves_num;			//number of leaves
+	float leaf_size;		//size of leaves
+	float rain_intensity;	//intensity of rain
+	float sun_intensity;	//intensity of sun
+	float snow_intensity;	//intensity of snow
+	float wind_intensity;	//intensity of wind
+	int flower_num;			//number of flowers
+
+public:
+
 };
 
+
+/*
+add colors, figure out how to implement seasons.
+current idea for seasons: assign seasons to numbers: 0 for sunny, 1 for snowing, 2 for snow
+
+*/
