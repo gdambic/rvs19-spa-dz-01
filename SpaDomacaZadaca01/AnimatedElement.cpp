696fc6130a04e6543c927e3fb49b191376042291
DynamiteBob17 <jan.mlinaric@gmail.com>
2024-03-24 17:58:12 +0100

    refactor: Source.cpp curly braces

--- SpaDomacaZadaca01/AnimatedElement.cpp
+++
@@ -1,5 +0,0 @@
-#include "AnimatedElement.h"
-#include "Scene.h"
-
-const int AnimatedElement::WINDOW_WIDTH = Scene::WINDOW_WIDTH;
-const int AnimatedElement::WINDOW_HEIGHT = Scene::WINDOW_HEIGHT;
--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,19 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include "Scene.h"
 
-int main()
-{
+int main() {
 	sf::RenderWindow window(sf::VideoMode(Scene::WINDOW_WIDTH, Scene::WINDOW_HEIGHT), "Hello, SFML world!");
 	window.setFramerateLimit(60);
 	Scene scene(&window);
 
 	sf::Clock clock;
 
-	while (window.isOpen())
-	{
+	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
