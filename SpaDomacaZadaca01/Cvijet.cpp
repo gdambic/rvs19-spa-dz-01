c0fc8c2d2d22607aa45b572e5044ec1810f93e10
Luka <87535465+LuxanL@users.noreply.github.com>
2024-03-24 20:17:08 +0100

    Segmented parts of the code to functions

--- SpaDomacaZadaca01/Cvijet.cpp
+++ SpaDomacaZadaca01/Cvijet.cpp
@@ -1,6 +1,34 @@
 #include "Cvijet.h"
 #include <vector>
 
+RectangleShape Cvijet::drawBg(Color color, Vector2f size)
+{
+	RectangleShape bg(size);
+	bg.setFillColor(color);
+
+	return bg;
+}
+
+CircleShape Cvijet::drawPetal(Color color, float size, Vector2f position, Vector2f scale)
+{
+
+	CircleShape petal(size);
+	petal.setFillColor(color);
+	petal.scale(scale);
+	petal.move(position);
+
+
+	return petal;
+}
+
+//CircleShape Cvijet::drawPetal()
+//{
+//
+//
+//
+//
+//}
+
 Cvijet::Cvijet(RenderWindow* window)
 {
 	this->window = window;
@@ -9,23 +37,38 @@ Cvijet::Cvijet(RenderWindow* window)
 
 void Cvijet::draw()
 {
-	Vector2f corePosition = { 200.f, 170.f };
+	//draw field
+
+	Color grassColor = { 17,124,19 };
+	Vector2f grassSize = { 1000.f, 1000.f };
+	window->draw(drawBg(grassColor, grassSize));
+
+	//draw sky
+	Color skyColor = { 135, 206, 235 };
+	Vector2f skySize = { 1000.f, 200.f };
+	window->draw(drawBg(skyColor, skySize));
+
+
+
+
+	//draw petals
+
+	Color petalColor = { 250, 224, 5 };
+	Vector2f petalPosition = { 200.f, 170.f };
 	Vector2f petalScale = { 1.f, 8.f };
-
-
 
 	for (size_t i = 0; i < 30; i++)
 	{
-		CircleShape petal(10.f);
-		petal.setFillColor(Color(240, 224, 55));
-		petal.scale(petalScale);
-		petal.move(corePosition);
-		petal.rotate(3 * (4*i));
+		CircleShape petal = drawPetal(petalColor, 10.f, petalPosition, petalScale);
+		petal.rotate(3 * (4 * i));
 		window->draw(petal);
 	}
 
-	CircleShape core(80.f);
-	core.move(Vector2f(120.f, 90.f));
-	core.setFillColor(Color(46, 41, 17));
-	window->draw(core);
+	//draw pistil
+
+	CircleShape pistil(80.f);
+	pistil.move(Vector2f(120.f, 90.f));
+	pistil.setFillColor(Color(46, 41, 17));
+	window->draw(pistil);
+
 }
--- SpaDomacaZadaca01/Cvijet.h
+++ SpaDomacaZadaca01/Cvijet.h
@@ -7,6 +7,11 @@ class Cvijet
 {
 private:
 	RenderWindow* window;
+
+	RectangleShape drawBg(Color color, Vector2f size);
+
+	CircleShape drawPetal(Color color, float size, Vector2f position, Vector2f scale);
+
 public:
 	Cvijet(RenderWindow* window);
 	void draw();
