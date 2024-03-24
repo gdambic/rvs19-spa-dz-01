f31121ce47a8a37013e97b4d69a0a954b1e8a66a
DynamiteBob17 <jan.mlinaric@gmail.com>
2024-03-24 17:49:23 +0100

    refactor: remove window constants from AnimatedElement class

--- SpaDomacaZadaca01/AnimatedElement.h
+++ SpaDomacaZadaca01/AnimatedElement.h
@@ -8,13 +8,11 @@ protected:
 	sf::Vector2f velocity;
 
 public:
-	static const int WINDOW_WIDTH;
-	static const int WINDOW_HEIGHT;
-
 	AnimatedElement(
 		const sf::Vector2f& position,
 		const sf::Vector2f& velocity
-	) : position(position), velocity(velocity) {}
+	) : position(position), velocity(velocity) {
+	}
 	virtual void draw(sf::RenderWindow& window) = 0;
 	virtual void update(const sf::Time& deltaTime) = 0;
 };
