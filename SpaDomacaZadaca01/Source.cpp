94433ff18f04d3052047f151bc762521e13e90e3
unknown <lzidaric@algebra.hr>
2024-03-27 09:52:37 +0100

    Zavrsena domaca zadaca

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,24 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+using namespace sf;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
-	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+    RenderWindow window(VideoMode(800, 600), "Moj cvijet");
+    window.setFramerateLimit(60);
+    Cvijet cvijet(&window);
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
 
-		window.clear();
-		//cvijet.draw();
-		window.display();
-	}
 
-	return 0;
+        window.clear();
+        cvijet.crtaj();
+        window.display();
+    }
+
+    return 0;
 }
